0e0686f770c336b27b39777102241dbd24e53c46
li4 <li4@125.com>
2019-07-02 23:02:07 +0800

    Initial commit

---
+++ src/main/scala/com/yang/Demo.scala
@@ -0,0 +1,17 @@
+package com.yang
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Demo {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setAppName("WordCount").setMaster("local")
+    val sc = new SparkContext(sparkConf)
+    //val ss: RDD[Int] = sc.makeRDD(List(1,2,3,4,5))
+    //val ss:RDD[Int] = sc.parallelize(List(1,2,3,4))
+    val ss:RDD[String] = sc.textFile("input")
+    ss.collect().foreach(println)
+
+  }
+
+}
